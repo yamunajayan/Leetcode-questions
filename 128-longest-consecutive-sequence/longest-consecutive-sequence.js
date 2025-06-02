5baae598688fc354f73de67f68cfd005ed8d3be8
yamunajayan <yamunajayan@gmail.com>
2025-06-01 22:19:46 -0400

    Time: 35 ms (86.25%) | Memory: 79.5 MB (82.34%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    //hashMap
+
+    const hashSet = new Set(nums);
+
+    let maxLength = 0;
+
+    hashSet.forEach(num => {
+        //check if previous number is there or not
+        if (!hashSet.has(num-1)) {
+            let currentNum = num;
+            currentLength = 1;
+
+            while (hashSet.has(currentNum+1)) {
+                currentLength+=1;
+                currentNum += 1;
+            }
+
+            maxLength = Math.max(currentLength, maxLength)
+
+        }
+    })
+    return maxLength; 
+  
+};
