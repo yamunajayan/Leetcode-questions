a8ecc69924324e2225b5d72509253450a1db47cf
yamunajayan <yamunajayan@gmail.com>
2025-06-05 16:24:13 -0400

    Time: 1 ms (96.79%) | Memory: 64.6 MB (87.70%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -8,25 +8,29 @@ var maxProfit = function(prices) {
     if(n===0 || n===1) return 0;
 
     //left=0
-    let left=0, right =1;
-    let maxProfit = 0
+    let min=prices[0];
+    let maxProfit = 0;
     //right=1
     //max_profit = 0
 
     //LOOP THROUGH prices:
-    while (right<n) {
-        //check if number[left]<num[right]:
-        if(prices[left] < prices[right]) {
-            let currentProfit = prices[right]-prices[left];
-            // is less min and max_profit < current profiti //update maxprofit
-            maxProfit = Math.max(currentProfit, maxProfit);
+    for(let i=0; i<n; i++) {
+
+        if (prices[i]<min) min=prices[i];
+        let currentProfit = prices[i]-min;
+        maxProfit = Math.max(currentProfit, maxProfit);
+        // //check if number[left]<num[right]:
+        // if(prices[i] < prices[right]) {
+        //     let currentProfit = prices[right]-prices[left];
+        //     // is less min and max_profit < current 
+        //maxProfit = Math.max(currentProfit, maxProfit);
             
-        } else {
-            //if not:
-            //left = right
-            left = right;
-        }
-        right += 1
+        // } else {
+        //     //if not:
+        //     //left = right
+        //     left = right;
+        // }
+        // right += 1
     }
 
     return maxProfit;
