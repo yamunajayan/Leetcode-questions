b77e18217a7bd6d9ac6a60625da648af7ceda617
yamunajayan <yamunajayan@gmail.com>
2025-09-25 15:12:34 -0400

    Time: 5 ms (95.68%) | Memory: 56.1 MB (84.66%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -7,19 +7,17 @@ var isAnagram = function(s, t) {
 
     if (s.length !== t.length) return false;
 
-    let sHashMap = new Map();
-    let tHashMap = new Map();
+    let count = {};
 
-    for (let i=0; i< s.length; i++) {
-        sHashMap.set(s[i], (sHashMap.get(s[i]) || 0) +1);
-        tHashMap.set(t[i], (tHashMap.get(t[i]) || 0) +1);
+    for (let i=0; i<s.length; i++) {
+        count[s[i]] = (count[s[i]] || 0) +1;
+        count[t[i]] = (count[t[i]] || 0) -1;
     }
 
-    for (let [key, val] of sHashMap) {
-        if (tHashMap.get(key) !== val) {
-            return false
-        }
+    for (let key in count) {
+        if (count[key] !== 0) return false
     }
+
     return true
     
 };
