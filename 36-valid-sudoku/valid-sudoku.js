ad5884a9b24d2814f16ff8632a476ee82d4b1846
yamunajayan <yamunajayan@gmail.com>
2026-01-13 20:07:54 -0500

    Time: 5 ms (59.52%) | Memory: 56.7 MB (68.72%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.js
+++ 36-valid-sudoku/valid-sudoku.js
@@ -3,31 +3,28 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-        if(board.length !== 9) return false;
-    
-    let m=0;
-    let columnMap = new Map();
-    let boxes = Array.from({length:9}, () =>new Set());
-    
-    while(m<board.length) {        
-        let rowSet = new Set();
-        row = board[m];
-        for (let i=0; i<row.length; i++) {
-            if(rowSet.has(row[i]) || row[i]>9 || row[i]<1) return false;
-            if(columnMap.get(i) && columnMap.get(i).includes(row[i])) return false            
-            let boxIndex = Math.floor(m/3)*3 + Math.floor(i/3);
-            if(boxes[boxIndex].has(row[i])) return false;
+    if(board.length !== 9) return false;
+
+    let rows = Array.from({length:9}, ()=>new Set());
+    let cols = Array.from({length:9}, ()=> new Set());
+    let boxes = Array.from({length:9}, ()=>new Set());
+
+    for (let r=0; r<9; r++) {
+        for (let c=0; c<9; c++) {
+                        let value = board[r][c];
+            if (value === ".") continue;
             
-            if(row[i]>=1 && row[i]<=9) {
-                rowSet.add(row[i]);
-                columnMap.set(i, [...(columnMap.get(i) ?? []), row[i]]);
-                boxes[boxIndex].add(row[i]);
-            }
+            if (value>9 || value <1) return false;
+            let boxIndex = Math.floor(r/3)*3 + Math.floor(c/3)
+            if (rows[r].has(value) || cols[c].has(value) || boxes[boxIndex].has(value)) return false;
             
+            rows[r].add(value);
+            cols[c].add(value);
+            boxes[boxIndex].add(value);
         }
-        m++;
-        console.log(columnMap);
     }
+    
+
     
     return true; 
 };
