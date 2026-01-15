fba141a62a6d3bcec23c1ea5310c66d31395b95e
yamunajayan <yamunajayan@gmail.com>
2026-01-14 23:45:09 -0500

    Time: 1 ms (84.86%) | Memory: 63.2 MB (94.88%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -4,11 +4,23 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    k = k % nums.length;
-    if (nums.length <= 1 || k===0) return nums;
-    nums.splice(0, nums.length, 
-        ...nums.slice(-k), 
-        ...nums.slice(0, nums.length - k)
-    );
+    let n=nums.length;
+    
+    k = k % n;
+    
+    reverse(0, n-1, nums);
+    reverse(0,k-1, nums);
+    reverse(k, n-1, nums);
+    return nums;
     
 };
+
+var reverse = function(i, j, arr) {
+    while(i<j) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        i++;
+        j--
+    }
+}
